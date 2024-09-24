de54128ab20d5a429ee9873baaca43aaea88075b
Mikey <56313858+shmily101@users.noreply.github.com>
2024-09-24 12:32:51 +0000

    feat: 添加路由

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template><div></div></template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware:
+    function (to, from) {
+      // 自定义内联中间件 跳转到首页
+      return navigateTo('/home', { replace: true })
+    },
+});
+</script>
