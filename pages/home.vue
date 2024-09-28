7bfdf2808d9164159bef2f0264f09a0e929e7b29
shmily101 <137143895@qq.com>
2024-09-28 21:40:56 +0800

    feat: 第二屏

--- pages/home.vue
+++ pages/home.vue
@@ -11,16 +11,81 @@
        <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
     </section>
 
-    <section id="section-card" relative min-h-100vh h-auto bg="#FAFAFC cover" flex text-gray items-center>
-       <div class="section-wrapper" m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
+    <section id="section-card" relative max-h-100vh h-auto bg="#FAFAFC cover" flex text-gray items-center>
+      <div m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
         <img w-screen h-screen block object-cover src="~/assets/img/home-s2-bg.png" />
+
         <div w-screen h-max flex="~ col" items-center absolute left-0 top-50% transform -translate-y-50%>
           <h1 text-60 mb-14>定制化Agent服务</h1>
-          <p text-24 d="0 60px 0">我们的定制化Agent服务，利用人工智能，赋能各行各业</p>
-          // todo cards
-          <div></div>
+          <p text="24 #808080" d="0 60px 0">我们的定制化Agent服务，利用人工智能，赋能各行各业</p>
+
+          <div id="section-scroll-card" w-screen overflow-hidden mt-60>
+            <div translate-x-100%>
+              <div w-3675 translate-x--100%>
+                <div id="section-scroll-card-top" flex class="section-picture">
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div mt-24>
+              <div w-3675>
+                <div id="section-scroll-card-bottom" flex class="section-picture">
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-agent" min-h-100vh h-auto bg="black cover" flex text-white items-center font-medium>
+       <div class="section-wrapper" w-920 m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
+        <img w-644 h-full block src="~/assets/img/home-s1-bg.png" />
+        <div w-full flex="~ col" items-center justify-center relative z-20>
+          <h1 class="section-agent-gradient section-name" font-extrabold text-72 tracking-wider mb-14 leading-tight transition-duration-1s op-0>Agent Power</h1>
+          <p class="section-slogan" font-bold text-36 d="0 60px 0" transition="duration-0.5s delay-0.5s" op-0>通过AI驱动的定制解决方案，让您的业务智能化和高效化</p>
         </div>
        </div>
+       <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
+    </section>
+
+    <section id="section-agent" min-h-100vh h-auto bg="black cover" flex text-white items-center font-medium>
+       <div class="section-wrapper" w-920 m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
+        <img w-644 h-full block src="~/assets/img/home-s1-bg.png" />
+        <div w-full flex="~ col" items-center justify-center relative z-20>
+          <h1 class="section-agent-gradient section-name" font-extrabold text-72 tracking-wider mb-14 leading-tight transition-duration-1s op-0>Agent Power</h1>
+          <p class="section-slogan" font-bold text-36 d="0 60px 0" transition="duration-0.5s delay-0.5s" op-0>通过AI驱动的定制解决方案，让您的业务智能化和高效化</p>
+        </div>
+       </div>
+       <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
+    </section>
+
+    <section id="section-agent" min-h-100vh h-auto bg="black cover" flex text-white items-center font-medium>
+       <div class="section-wrapper" w-920 m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
+        <img w-644 h-full block src="~/assets/img/home-s1-bg.png" />
+        <div w-full flex="~ col" items-center justify-center relative z-20>
+          <h1 class="section-agent-gradient section-name" font-extrabold text-72 tracking-wider mb-14 leading-tight transition-duration-1s op-0>Agent Power</h1>
+          <p class="section-slogan" font-bold text-36 d="0 60px 0" transition="duration-0.5s delay-0.5s" op-0>通过AI驱动的定制解决方案，让您的业务智能化和高效化</p>
+        </div>
+       </div>
+       <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
     </section>
   </div>
 </template>
@@ -40,8 +105,36 @@ const agentFn = () => {
   });
 };
 
+const initScrollAnimation = () => {
+  const card = document.getElementById("section-scroll-card"),
+    endHeight = window.innerHeight + card.clientHeight;
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: card,
+        start: "top 100%",
+        end: "+=" + endHeight,
+        scrub: 1, // 调整scrub时间
+      },
+    })
+    .to("#section-scroll-card-top", {
+      ease: "none",
+      xPercent: 50,
+    })
+    .to(
+      "#section-scroll-card-bottom",
+      {
+        ease: "none",
+        xPercent: -50,
+      },
+      "<"
+    );
+};
+
 onMounted(() => {
   agentFn()
+  initScrollAnimation();
 })
 
 </script>
@@ -65,6 +158,20 @@ onMounted(() => {
     .section-slogan {
       transform: translate3d(0, 0, 0);
       opacity: 1;
+    }
+  }
+}
+
+#section-card {
+  .section-picture {
+    img {
+      display: block;
+      flex-shrink: 0;
+      width: 387px;
+    }
+
+    img + img {
+      margin-left: 24px;
     }
   }
 }
