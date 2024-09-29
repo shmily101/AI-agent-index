ac8530d204deccf73792c4c662f3054850d03504
Mikey <56313858+shmily101@users.noreply.github.com>
2024-09-29 10:23:33 +0000

    feat: 为什么选择我们+FAQ初始化

--- pages/home.vue
+++ pages/home.vue
@@ -11,8 +11,8 @@
        <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
     </section>
 
-    <section id="section-card" relative max-h-100vh h-auto bg="#FAFAFC cover" flex text-gray items-center>
-      <div m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
+    <section id="section-card" relative max-h-100vh h-auto bg="#FAFAFC cover" text-gray>
+      <div m="y-0 auto" transition="duration-1s delay-0.5s">
         <img w-screen h-screen block object-cover src="~/assets/img/home-s2-bg.png" />
 
         <div w-screen h-max flex="~ col" items-center absolute left-0 top-50% transform -translate-y-50%>
@@ -23,30 +23,30 @@
             <div translate-x-100%>
               <div w-3675 translate-x--100%>
                 <div id="section-scroll-card-top" flex class="section-picture">
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
                 </div>
               </div>
             </div>
             <div mt-24>
               <div w-3675>
                 <div id="section-scroll-card-bottom" flex class="section-picture">
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
-                  <img class="lazyload" src="~/assets/img/s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
+                  <img class="lazyload" src="~/assets/img/home-s2-card-01.png" alt="" />
                 </div>
               </div>
             </div>
@@ -55,37 +55,50 @@
       </div>
     </section>
 
-    <section id="section-agent" min-h-100vh h-auto bg="black cover" flex text-white items-center font-medium>
-       <div class="section-wrapper" w-920 m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
-        <img w-644 h-full block src="~/assets/img/home-s1-bg.png" />
-        <div w-full flex="~ col" items-center justify-center relative z-20>
-          <h1 class="section-agent-gradient section-name" font-extrabold text-72 tracking-wider mb-14 leading-tight transition-duration-1s op-0>Agent Power</h1>
-          <p class="section-slogan" font-bold text-36 d="0 60px 0" transition="duration-0.5s delay-0.5s" op-0>通过AI驱动的定制解决方案，让您的业务智能化和高效化</p>
+    <section id="section-us-one" bg="#FAFAFC cover" flex="~ col" text-gray items-center>
+        <h1 mt-160 text-40 font-300>为什么选择我们？</h1>
+        <div class="fade-trigger fade-copy" flex items-center w-1090 mx-auto mt-80>
+          <h2 text-50 font-medium>技术领先</h2>
+          <p ml-104 lh-normal text="18 #808080">依托最前沿的自然语言处理与大型语言模型技术，我们确保每个项目<br />的技术先进性。</p>
         </div>
-       </div>
-       <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
+        <img class="fade-trigger fade-copy" w-1235 h-600 mx-auto mt-30 border-rd-30px src="~/assets/img/home-s3-01-poster.jpg" />
     </section>
 
-    <section id="section-agent" min-h-100vh h-auto bg="black cover" flex text-white items-center font-medium>
-       <div class="section-wrapper" w-920 m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
-        <img w-644 h-full block src="~/assets/img/home-s1-bg.png" />
-        <div w-full flex="~ col" items-center justify-center relative z-20>
-          <h1 class="section-agent-gradient section-name" font-extrabold text-72 tracking-wider mb-14 leading-tight transition-duration-1s op-0>Agent Power</h1>
-          <p class="section-slogan" font-bold text-36 d="0 60px 0" transition="duration-0.5s delay-0.5s" op-0>通过AI驱动的定制解决方案，让您的业务智能化和高效化</p>
+    <section id="section-us-two" bg="#FAFAFC cover" flex="~ col" text-gray items-center pt-200>
+        <div class="fade-trigger fade-copy" flex items-center w-1090 mx-auto>
+          <h2 text-50 font-medium>深度定制</h2>
+          <p ml-104 lh-normal text="18 #808080">我们根据客户的具体需求量身定制解决方案，确保与业务目标紧密<br />契合。</p>
         </div>
-       </div>
-       <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
+        <img class="fade-trigger fade-copy" w-1235 h-600 mx-auto mt-30 border-rd-30px src="~/assets/img/home-s3-01-poster.jpg" />
     </section>
 
-    <section id="section-agent" min-h-100vh h-auto bg="black cover" flex text-white items-center font-medium>
-       <div class="section-wrapper" w-920 m="y-0 auto" flex="~ col" items-center transition="duration-1s delay-0.5s">
-        <img w-644 h-full block src="~/assets/img/home-s1-bg.png" />
-        <div w-full flex="~ col" items-center justify-center relative z-20>
-          <h1 class="section-agent-gradient section-name" font-extrabold text-72 tracking-wider mb-14 leading-tight transition-duration-1s op-0>Agent Power</h1>
-          <p class="section-slogan" font-bold text-36 d="0 60px 0" transition="duration-0.5s delay-0.5s" op-0>通过AI驱动的定制解决方案，让您的业务智能化和高效化</p>
+    <section id="section-us-three" bg="#FAFAFC cover" flex="~ col" text-gray items-center pt-200>
+        <div class="fade-trigger fade-copy" flex items-center w-1090 mx-auto>
+          <h2 text-50 font-medium>全流程支持</h2>
+          <p ml-104 lh-normal text="18 #808080">从概念设计、开发、到后续的维护与优化，我们提供全流程的支持<br />服务，确保您的AI系统长期稳定高效运行。</p>
         </div>
-       </div>
-       <p absolute bottom-40 left-50% transform -translate-x-50% text-16 >↓ 下滑查看</p>
+        <img class="fade-trigger fade-copy" w-1235 h-600 mx-auto mt-30 border-rd-30px src="~/assets/img/home-s3-01-poster.jpg" />
+    </section>
+
+    <section id="section-us-four" bg="#FAFAFC cover" flex="~ col" text-gray items-center py-200>
+        <div class="fade-trigger fade-copy" flex items-center w-1090 mx-auto>
+          <h2 text-50 font-medium>业务增长驱动</h2>
+          <p ml-104 lh-normal text="18 #808080">通过智能化解决方案，帮助您提升业务效率、降低成本，并<br />增强市场竞争力。</p>
+        </div>
+        <img class="fade-trigger fade-copy" w-1235 h-600 mx-auto mt-30 border-rd-30px src="~/assets/img/home-s3-01-poster.jpg" />
+    </section>
+
+    <section id="section-FAQ" min-h-100vh h-auto bg="black cover" text-white items-center font-medium relative>
+      <img w-screen h-screen block src="~/assets/img/home-s7-bg.png" />
+      
+      <div absolute left-0 top-50% transform -translate-y-50% m="y-0 auto" flex items-center px-170 transition="duration-1s delay-0.5s">
+        <div text-24 flex="~ col" items-center>
+          <h1 text-72>您可能想知道</h1>
+          <p mt-24>这里没有想要了解的问题？您可以现在就<span text-primary font-bold mx-8>联系我们</span>！</p>
+          <p mt-8>我们将为您提供免费的咨询服务。</p>
+        </div>
+        <div>cardlist</div>
+      </div>
     </section>
   </div>
 </template>
@@ -132,14 +145,43 @@ const initScrollAnimation = () => {
     );
 };
 
+const triggerFn = () => {
+  const triggerList = document.querySelectorAll(".fade-trigger");
+  triggerList.forEach((item) => {
+    const hook = item.getAttribute("data-hook") || "70%";
+    console.log(hook, "hook");
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: item,
+        start: "top " + hook,
+        toggleClass: "active",
+        markers: false,
+      },
+    });
+  });
+  console.log(triggerList)
+};
+
 onMounted(() => {
   agentFn()
   initScrollAnimation();
+  triggerFn();
 })
 
 </script>
   
 <style lang="scss" scoped>
+  .fade-copy {
+    transition: opacity 0.5s, transform 0.5s;
+    transform: translateY(50px);
+    opacity: 0;
+    &.active {
+      transform: translateY(0px);
+      opacity: 1;
+    }
+  }
+
+
 #section-agent {
   .section-agent-gradient {
     background-image: linear-gradient(117.99deg, rgba(42, 130, 228, 1) 0%, rgba(52, 120, 247, 1) 0%, rgba(155, 210, 250, 1) 62.87%, rgba(245, 255, 250, 1) 100%);
