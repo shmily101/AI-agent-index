59c7f88675e1d3704b1c792f8cf1ee16c337beac
Mikey <56313858+shmily101@users.noreply.github.com>
2024-10-11 12:30:08 +0000

    fix: 更改菜单样式

--- components/TopMenu.vue
+++ components/TopMenu.vue
@@ -1,17 +1,37 @@
 <template>
-  <nav font-normal flex justify-between fixed top-20 left-30 right-30 p="y-10 x-30" bg-white border="[4px_solid_rgba(242,243,245,1)] rd-20" z-1000 text-gray>
-    <img src="~/assets/svg/logo.svg" />
-    <ul flex justify-center items-center ml-10>
-      <li mr-40 hover:text-primary><NuxtLink to="/">首页</NuxtLink></li>
-      <li mr-40 hover:text-primary><NuxtLink to="/service">服务</NuxtLink></li>
-      <li mr-40 hover:text-primary><NuxtLink to="/">行业</NuxtLink></li>
-      <li mr-40 hover:text-primary><NuxtLink to="/">关于我们</NuxtLink></li>
+  <nav
+    flex
+    justify-between
+    items-center
+    fixed
+    top-0
+    left-0
+    p="x-80 y-10"
+    w-screen
+    bg-white
+    z-1000
+    text="gray"
+    class="keep-px-nav"
+  >
+    <img w-60 h-60 src="~/assets/svg/logo.svg" />
+    <ul flex gap-40 justify-center items-center>
+      <li hover:text-primary><NuxtLink to="/">首页</NuxtLink></li>
+      <li hover:text-primary><NuxtLink to="/service">服务</NuxtLink></li>
+      <li hover:text-primary><NuxtLink to="/">行业</NuxtLink></li>
+      <li hover:text-primary><NuxtLink to="/">关于我们</NuxtLink></li>
       <li hover:text-primary><NuxtLink to="/">新闻资讯</NuxtLink></li>
     </ul>
-    <button w-121 h-39 py-2 rounded-full bg-primary text-white>联系我们</button>
+    <button w-121 h-40 py-2 rounded-full bg-primary text-white>联系我们</button>
   </nav>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
 </script>
+
+<style lang="scss" scoped>
+.keep-px-nav {
+  height: 65px;
+  font-size: 14px;
+}
+</style>
