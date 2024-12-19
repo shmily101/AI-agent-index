90d7a43d6327857ce2ee2661f875f727b3dfd904
Mikey <56313858+shmily101@users.noreply.github.com>
2024-12-19 08:21:55 +0000

    feat: 重构问答卡片

--- pages/home.vue
+++ pages/home.vue
@@ -70,50 +70,31 @@
       <img class="fade-trigger" w-1235 h-600 mx-auto mt-30 border-rd-30px src="~/assets/img/home-s3-04-poster.png" />
     </section>
 
-    <section id="section-FAQ" bg="#000 cover" p="t-300 b-200" text-white items-center font-medium>
-      <div class="fade-trigger" m="y-0 auto" flex items-center px-170 min-h-500 transition="duration-1s delay-0.5s">
-        <div text-24 flex="~ col" items-center>
-          <h1 text-72>您可能想知道</h1>
-          <p text="#E5E5E5" mt-24>
-            这里没有想要了解的问题？您可以现在就<span text="#326CF9" font-bold mx-10><NuxtLink to="/contact">联系我们</NuxtLink></span
-            >！
-          </p>
-          <p text="#E5E5E5" mt-8>我们将为您提供免费的咨询服务。</p>
-        </div>
-        <div flex="~ col" text-18 ml-200>
-          <div
-            v-for="(card, index) in cards"
-            :key="index"
-            w-700
-            bg="#212121 cover"
-            shadow="[0px_3px_49px_9px_rgba(0,0,0,0.06)]"
-            backdrop-blur-20px
-            text-white
-            m-15
-            border-rd-10
-            cursor-pointer
-            overflow-hidden
-            max-h-90px
-            :class="['card', { expanded: card.expanded }]"
-            @click="toggleCard(index)"
-          >
-            <div flex justify-between items-center m="x-25 y-15">
-              <span font-800 text="20">{{ card.question }}</span>
-              <span>
-                <img v-show="card.expanded" w-20 src="~/assets/svg/chevron-up.svg" />
-                <img v-show="!card.expanded" w-20 src="~/assets/svg/chevron-down.svg" />
-              </span>
-            </div>
-            <!-- eslint-disable-next-line vue/no-v-html -->
-            <div v-if="card.expanded" m="x-25 y-15" transition="opacity duration-0.3s" v-html="card.answer" />
-          </div>
-        </div>
+    <section id="section-FAQ" bg="#000" p="t-50 x-50">
+      <div text="#fff 18" flex="~ col" items-center m="y-100">
+        <h1 text-40 font="300 sans">您可能想知道</h1>
+        <p text="#E5E5E5" mt-24 font="300 sans">
+          这里没有想要了解的问题？您可以现在就<span text="#326CF9" font-bold mx-10><NuxtLink to="/contact">联系我们</NuxtLink></span
+          >！我们将为您提供免费的咨询服务。
+        </p>
       </div>
+      <ul id="card-list" w-full>
+        <li v-for="(card, index) in cards" :key="index" class="card-item" border="b-1 gray solid" op-0 translate-y-60>
+          <button text="32" w-full p="x-60 y-35" flex justify-between items-center hover:bg-gray-50 transition-colors duration-200 @click="toggleCard(index)">
+            <span font-medium text="#fff">{{ card.question }}</span>
+            <!-- <ChevronDownIcon w-5 h-5 text-gray-500 transition-transform duration-200 :class="{ 'transform rotate-180': card.expanded }" /> -->
+          </button>
+          <transition @enter="enter" @after-enter="afterEnter" @leave="leave">
+            <div v-show="card.expanded" overflow-hidden p="x-60">
+              <div pb-20 text="#ccc 22" line-height-loose v-html="card.answer" />
+            </div>
+          </transition>
+        </li>
+      </ul>
     </section>
 
     <section id="section-user" w-screen relative bg="black cover" py-200>
       <img w-screen block src="~/assets/img/home-s8-bg.png" />
-
       <div flex="~ col" w-screen tracking-wide items-center absolute left="50%" top="50%" transform="translate-y--50% translate-x--50%" text="white 62">
         <p id="text-one" />
         <p id="text-two" mt-15 />
@@ -164,6 +145,7 @@ const initScrollAnimation = () => {
       '<',
     )
 }
+
 // 第四屏-问答卡片
 const cards = ref([
   {
@@ -180,22 +162,74 @@ const cards = ref([
   {
     question: 'AI Agent适合哪些行业和场景？',
     answer:
-      'AI Agent具有广泛的应用场景，我们已为以下行业提供定制化解决方案：<div style="padding: 20px"><br />● 金融行业：自动化客户支持、合规检查、风险分析；<br />● 医疗与健康行业：智能诊断助手、患者服务、健康数据分析；<br />● 教育行业：个性化学习助手、在线教学支持、知识管理；<br />● 零售与电子商务行业：个性化推荐、订单管理、售后服务；<br />● 物流与供应链行业：运输调度优化、库存管理、实时物流跟踪；<br />● 游戏与娱乐行业：NPC交互设计、玩家行为分析、个性化内容推荐；<br />● 客户服务与HR行业：招聘助手、员工支持、客户关系管理；<br />● 法律与合规行业：文档审查、法律咨询助手、合规检查；<br />● 环境与能源行业：能耗分析、环境监测、智能调度管理。</div><br />无论您的行业是什么，我们都能为您量身定制适合的AI Agent，助力您的业务升级。',
+      'AI Agent具有广泛的应用场景，我们已为以下行业提供定制化解决方案：<div><br />● <b style="color: chartreuse;">金融行业</b>：自动化客户支持、合规检查、风险分析；<br />● <b style="color: chartreuse;">医疗与健康行业</b>：智能诊断助手、患者服务、健康数据分析；<br />● <b style="color: chartreuse;">教育行业</b>：个性化学习助手、在线教学支持、知识管理；<br />● <b style="color: chartreuse;">零售与电子商务行业</b>：个性化推荐、订单管理、售后服务；<br />● <b style="color: chartreuse;">物流与供应链行业</b>：运输调度优化、库存管理、实时物流跟踪；<br />● <b style="color: chartreuse;">游戏与娱乐行业</b>：NPC交互设计、玩家行为分析、个性化内容推荐；<br />● <b style="color: chartreuse;">客户服务与HR行业</b>：招聘助手、员工支持、客户关系管理；<br />● <b style="color: chartreuse;">法律与合规行业</b>：文档审查、法律咨询助手、合规检查；<br />● <b style="color: chartreuse;">环境与能源行业</b>：能耗分析、环境监测、智能调度管理。</div><br />无论您的行业是什么，我们都能为您量身定制适合的AI Agent，助力您的业务升级。',
     expanded: false,
   },
-  { question: '如何为我的公司设计定制化的AI Agent？', answer: '为了确保您获得真正符合业务需求的定制化AI Agent，我们采用了一套科学高效的服务流程', expanded: false },
+  {
+    question: '如何为我的公司设计定制化的AI Agent？',
+    answer:
+      '为了确保您获得真正符合业务需求的定制化AI Agent，我们采用了一套科学高效的服务流程，每一步都以您的业务价值为核心设计：<div><b style="color: chartreuse;">（1）需求分析</b><br />我们的团队会与您深入沟通，全面了解您的项目目标和业务需求。我们相信，只有清晰理解您的行业背景和痛点，才能为您打造真正有效的AI解决方案。<br /><b style="color: chartreuse;">（2）模型选择</b><br />根据您的具体需求，我们会为您挑选最适合的AI模型或技术框架。这一步的核心是确保AI Agent能够与您的业务完美匹配，并具备足够的灵活性和适应性。<br /><b style="color: chartreuse;">（3）Agent开发</b><br />由经验丰富的Prompt工程师团队，设计并开发出专为您业务场景定制的AI Agent。无论是功能设计还是交互逻辑，都将围绕您的目标用户和实际应用场景量身打造。<br /><b style="color: chartreuse;">（4）微调优化</b><br />我们会使用您的业务数据，对AI模型进行深度微调，从而最大化地提升其性能与准确性。通过这一步，AI Agent将更加贴合您的业务需求，并能在复杂场景中表现出色。<br /><b style="color: chartreuse;">（5）测试验证</b><br />开发完成后，我们会对AI Agent进行严格的测试和验证，确保其功能、性能和稳定性都达到预期效果。我们相信，只有经过全面测试的解决方案，才能真正帮助您的业务取得成功。</div>',
+    expanded: false,
+  },
   {
     question: '如何开展合作？',
     answer: '您可以通过联系我们的团队进行初步沟通，我们会安排一次免费的需求分析会议，了解您的业务需求并为您提供初步建议。您只需点击“联系我们”按钮即可开始。',
     expanded: false,
   },
 ])
+// 卡片收起/展开
 const toggleCard = (index: number) => {
-  cards.value = cards.value.map((card, i) => ({
-    ...card,
-    expanded: i === index ? !card.expanded : false,
-  }))
+  cards.value[index].expanded = !cards.value[index].expanded
 }
+// 卡片展开/收起过渡动画
+const enter = (el: Element) => {
+  const element = el as HTMLElement
+  element.style.position = 'absolute'
+  element.style.height = 'auto'
+  const height = element.offsetHeight
+  element.style.position = ''
+  element.style.height = '0'
+  // 触发重绘
+  getComputedStyle(element).height
+  requestAnimationFrame(() => {
+    element.style.height = `${height}px`
+  })
+}
+const afterEnter = (el: Element) => {
+  const element = el as HTMLElement
+  element.style.height = 'auto'
+}
+const leave = (el: Element) => {
+  const element = el as HTMLElement
+  const height = element.offsetHeight
+  element.style.height = `${height}px`
+  // 触发重绘
+  getComputedStyle(element).height
+  requestAnimationFrame(() => {
+    element.style.height = '0'
+  })
+}
+
+// 卡片展示动画
+const cardFn = () => {
+  const cardList = document.getElementById('card-list')
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: cardList,
+        start: 'top 70%',
+        end: 'bottom 100%',
+        toggleActions: 'play none none reset',
+        markers: false,
+      },
+    })
+    .to('.card-item', {
+      opacity: 1,
+      y: 0,
+      stagger: 0.2,
+    })
+}
+
 // 第五屏-已使用用户
 const userFn = () => {
   gsap
@@ -227,6 +261,7 @@ onMounted(() => {
   agentFn()
   initScrollAnimation()
   triggerFn()
+  cardFn()
   userFn()
   // 更新ScrollTrigger，解决方案：https://gsap.com/community/forums/topic/33486-nuxt-3-gsap-scrolltrigger-startend-position-problem-after-route-navigation/
   // true: 延迟200ms再进行更新，参考：https://gsap.com/docs/v3/Plugins/ScrollTrigger/static.refresh()
@@ -276,5 +311,12 @@ onMounted(() => {
     background-color: #5886f4;
     max-height: 100vh;
   }
+}
+</style>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: height 0.3s ease;
 }
 </style>
